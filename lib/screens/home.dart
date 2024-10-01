85a0cbe2e6c191332c128f2d825df3397146d6db
Samuel Olayemi <sam_olayemi65@yahoo.com>
2024-10-01 16:52:04 +0100

    Fetch items from the server functionality

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_crud_api/screens/add_list.dart';
+import 'package:http/http.dart' as http;
 
 class Homepage extends StatefulWidget {
   const Homepage({super.key});
@@ -9,11 +12,35 @@ class Homepage extends StatefulWidget {
 }
 
 class _HomepageState extends State<Homepage> {
+  List items = [];
+  bool isLoading = true;
   @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Todo List")
+      ),
+      body: Visibility(
+        visible: isLoading,
+        child: Center(child: CircularProgressIndicator(),),
+        replacement: RefreshIndicator(
+          onRefresh: fetchTodo,
+          child: ListView.builder(
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              final item = items[index] as Map;
+              return ListTile(
+                leading: CircleAvatar(child: Text('${index + 1}'),),
+                title: Text(item['title']),
+                subtitle: Text(item['description']),
+              );
+            }
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: navigateToAddPage,
@@ -27,4 +54,21 @@ class _HomepageState extends State<Homepage> {
     );
     Navigator.push(context, route);
   }
+  Future<void> fetchTodo() async {
+    // ignore: prefer_const_declarations
+    final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await  http.get(uri);
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    } 
+    setState(() {
+      isLoading = false;
+    });
+  }
+
 } 
